153dcb2e64faf4a92fbec5e6e2d35dc14c17297a
JunKyu Park <pjk0007@naver.com>
2019-04-02 00:10:17 +0900

    second

--- sfp.c
+++ sfp.c
@@ -96,6 +96,9 @@ sfp float2sfp(float input){
 	else if(exp < -31){
 		return 0;
 	}
+	else if(exp == -31){
+
+	}
 	sign = sign >> 16;
 	exp = exp + 31;
 	exp = exp << 9;
@@ -133,6 +136,99 @@ float sfp2float(sfp input){
 }
 
 sfp sfp_add(sfp in1, sfp in2){
+	int sign[2], exp[2], frac[2];
+	int s, e, f;
+	int diff=0, cnt = 0;
+	sign[0] = in1 / 0x8000;
+	sign[1] = in2 / 0x8000;
+
+	exp[0] = in1 & 0x7e00;
+	exp[0] = exp[0] >> 9;
+	exp[0] = exp[0] - 31;
+	exp[1] = in2 & 0x7e00;
+	exp[1] = exp[1] >> 9;
+	exp[1] = exp[1] - 31;
+
+	frac[0] = (in1 & 0x01ff) + 0x0200;
+	frac[1] = (in2 & 0x01ff) + 0x0200;
+
+	if(sign[0] != sign[1]){
+		if(exp[0] > exp[1]){
+			e = exp[0];
+			s = sign[0];
+			diff = exp[0] - exp[1];
+			frac[1] = frac[1] >> diff;
+			f = frac[0] - frac[1];
+			while(f < 0x0200){
+				cnt++;
+				f << 1;
+			}
+			f = f - 0x0200;
+			e -= cnt;
+			return (s * 0x8000) + ((e + 31) << 9) + f;
+		}
+		else if(exp[0] < exp[1]){
+			e = exp[1];
+		       	s = sign[1];
+			diff = exp[1] - exp[0];
+			frac[0] = frac[0] >> diff;
+			f = frac[1] - frac[0];
+			while(f < 0x0200){
+				cnt++;
+				f << 1;
+			}
+			f = f - 0x0200;
+			e -= cnt;
+			return (s * 0x8000) + ((e + 31) << 9) + f;
+		}
+		else{
+			if(frac[0] > frac[1]){
+				e = exp[0];
+			       	s = sign[0];
+				f = frac[0] - frac[1];
+				while(f < 0x0200){
+					cnt++;
+					f << 1;
+				}
+				f = f - 0x0200;
+				e -= cnt;
+				return (s * 0x8000) + ((e + 31) << 9) + f;
+			}
+			else if(frac[0] < frac[1]){
+				e = exp[1];
+			       	s = sign[1];
+				f = frac[1] - frac[0];
+				while(f < 0x0200){
+					cnt++;
+					f << 1;
+				}
+				f = f - 0x0200;
+				e -= cnt;
+				return (s * 0x8000) + ((e + 31) << 9) + f;
+			}
+			else return 0;
+		}
+	}
+	else {
+		s = sign[0];
+		if(exp[0] > exp[1]){
+			e = exp[0];
+			diff = exp[0] - exp[1];
+			frac[1] = frac[1] >> diff;
+		}
+		else if(exp[1] >= exp[0]){
+			e = exp[1];
+			diff = exp[1] - exp[0];
+			frac[0] = frac[0] >> diff;
+		}
+		f = frac[0] + frac[1];
+		if(f >= 0x0400){
+			f >> 1;
+			e += 1;
+		}
+		f = f - 0x0200;
+		return (s * 0x8000) + ((e + 31) << 9) + f;
+	}
 }
 
 sfp sfp_mul(sfp in1, sfp in2){
